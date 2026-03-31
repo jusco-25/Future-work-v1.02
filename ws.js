cfc83dbb55f04ab29ac7509205b7583e6a8a1a61
jusco-25 <pangujeanjules@gmail.com>
2026-03-31 20:50:56 +0100

    Create ws.js

---
+++ ws.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'future-work-v102';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
